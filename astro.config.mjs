2542f6f0c17363233b842fbf6b03527a59f815be
Kevin C <thekevinchoi@gmail.com>
2026-02-23 02:11:55 -0800

    Add github actions for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://thekevinchoi.com",
+  site: "thekevinchoi.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
