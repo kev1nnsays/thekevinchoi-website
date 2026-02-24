9f5097fbe632a0be117a8c5c221d47b6546f993a
Kevin C <thekevinchoi@gmail.com>
2026-02-23 21:31:09 -0800

    try using github.io website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://www.thekevinchoi.com",
+  site: "http://kev1nnsays.github.io",
+  base: "/thekevinchoi-website",
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
